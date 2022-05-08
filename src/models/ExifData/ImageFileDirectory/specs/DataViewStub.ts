24512cf1145ea21824b754ecc1c438a2c8804305
Junyoung Yang <tom9744@gmail.com>
2022-05-09 00:19:40 +0900

    ImageFileDirectory 클래스 테스트 케이스 추가 - 정수형 데이터

--- src/models/ExifData/ImageFileDirectory/specs/DataViewStub.ts
+++ src/models/ExifData/ImageFileDirectory/specs/DataViewStub.ts
@@ -32,109 +32,83 @@ export class DataViewStub {
 
     this._entryOffset = this.BYTE_ALIGN_OFFSET + 2;
     this._dataOffset = this._entryOffset + maxEntryCount * this.BYTES_PER_ENTRY;
-
-    // this.dataView.setUint16(0, 0x010f); // Tag (= Make)
-    // this.dataView.setUint16(2, 0x0002); // Format (= ASCII String)
-    // this.dataView.setUint32(4, 0x0006); // Component Count
-    // this.dataView.setUint32(8, 0x0020); // Payload (= Data's Offset)
-
-    // this.dataView.setUint16(0, 0x0112); // Tag (= Orientation)
-    // this.dataView.setUint16(2, 0x0003); // Format (= Unsinged Short)
-    // this.dataView.setUint32(4, 0x0001); // Component Count
-    // this.dataView.setUint16(8, 0x0006); // Payload (= Data)
-
-    // this.dataView.setUint16(0, 0x8769); // Tag (= ExifOffset)
-    // this.dataView.setUint16(2, 0x0004); // Format (= Unsinged Long)
-    // this.dataView.setUint32(4, 0x0001); // Component Count
-    // this.dataView.setUint32(8, 0x00e0); // Payload (= Data)
-
-    // this.dataView.setUint16(0, 0x0211); // Tag (= YCbCrCoefficients)
-    // this.dataView.setUint16(2, 0x0005); // Format (= Unsigned Rational)
-    // this.dataView.setUint32(4, 0x0003); // Component Count
-    // this.dataView.setUint32(8, 0x0010); // Payload (= Data's Offset)
-
-    // this.dataView.setUint16(0, 0x0092); // Tag (= Random Number)
-    // this.dataView.setUint16(2, 0x0008); // Format (= Singed Short)
-    // this.dataView.setUint32(4, 0x0001); // Component Count
-    // this.dataView.setInt16(8, 0xfff4); // Payload (= Data)
-
-    // this.dataView.setUint16(0, 0x1769); // Tag (= Random Number)
-    // this.dataView.setUint16(2, 0x0009); // Format (= Unsinged Long)
-    // this.dataView.setUint32(4, 0x00000001); // Component Count
-    // this.dataView.setUint32(8, 0xfffffebf); // Payload (= Data)
-
-    // this.dataView.setUint16(0, 0x1211); // Tag (= Random Number)
-    // this.dataView.setUint16(2, 0x000a); // Format (= Unsigned Rational)
-    // this.dataView.setUint32(4, 0x0003); // Component Count
-    // this.dataView.setUint32(8, 0x0010); // Payload (= Data's Offset)
-
-    // this.dataView.setUint16(0, 0x9721); // Tag (= Random Number)
-    // this.dataView.setUint16(2, 0x000b); // Format (= Unsigned Rational)
-    // this.dataView.setUint32(4, 0x0004); // Component Count
-    // this.dataView.setUint32(8, 0x0010); // Payload (= Data's Offset)
-
-    // this.dataView.setUint16(0, 0x8631); // Tag (= Random Number)
-    // this.dataView.setUint16(2, 0x000c); // Format (= Unsigned Rational)
-    // this.dataView.setUint32(4, 0x0003); // Component Count
-    // this.dataView.setUint32(8, 0x0010); // Payload (= Data's Offset)
-
-    // this.dataView.setUint8(this.byteAlignOffset + 32, 0x0041); // A
-    // this.dataView.setUint8(this.byteAlignOffset + 33, 0x0070); // p
-    // this.dataView.setUint8(this.byteAlignOffset + 34, 0x0070); // p
-    // this.dataView.setUint8(this.byteAlignOffset + 35, 0x006c); // l
-    // this.dataView.setUint8(this.byteAlignOffset + 36, 0x0065); // e
-    // this.dataView.setUint8(this.byteAlignOffset + 37, 0x0000); // \0
   }
 
-  private setUint8ToEntry(value: number): void {
+  private setInt8ToEntry(value: number, signed: boolean = false): void {
     if (this._dataOffset + 1 > this.size) {
       throw new RangeError("지정된 DataView의 크기를 초과했습니다.");
     }
 
-    this._dataView.setUint8(this._entryOffset, value);
+    if (signed) {
+      this._dataView.setInt8(this._entryOffset, value);
+    } else {
+      this._dataView.setUint8(this._entryOffset, value);
+    }
     this._entryOffset++;
   }
 
-  private setUint16ToEntry(value: number): void {
+  private setInt16ToEntry(value: number, signed: boolean = false): void {
     if (this._dataOffset + 2 > this.size) {
       throw new RangeError("지정된 DataView의 크기를 초과했습니다.");
     }
 
-    this._dataView.setUint16(this._entryOffset, value);
+    if (signed) {
+      this._dataView.setInt16(this._entryOffset, value);
+    } else {
+      this._dataView.setUint16(this._entryOffset, value);
+    }
     this._entryOffset += 2;
   }
 
-  private setUint32ToEntry(value: number): void {
+  private setInt32ToEntry(value: number, signed: boolean = false): void {
     if (this._dataOffset + 4 > this.size) {
       throw new RangeError("지정된 DataView의 크기를 초과했습니다.");
     }
 
-    this._dataView.setUint32(this._entryOffset, value);
+    if (signed) {
+      this._dataView.setInt32(this._entryOffset, value);
+    } else {
+      this._dataView.setUint32(this._entryOffset, value);
+    }
     this._entryOffset += 4;
   }
 
-  private setUint8ToDataArea(value: number): void {
+  private setInt8ToDataArea(value: number, signed: boolean = false): void {
     if (this._dataOffset + 1 > this.size) {
       throw new RangeError("지정된 DataView의 크기를 초과했습니다.");
     }
 
-    this._dataView.setUint8(this._dataOffset, value);
+    if (signed) {
+      this._dataView.setInt8(this._dataOffset, value);
+    } else {
+      this._dataView.setUint8(this._dataOffset, value);
+    }
     this._dataOffset++;
   }
 
-  private setUint16ToDataArea(value: number): void {
+  private setInt16ToDataArea(value: number, signed: boolean = false): void {
     if (this._dataOffset + 2 > this.size) {
       throw new RangeError("지정된 DataView의 크기를 초과했습니다.");
     }
-    this._dataView.setUint16(this._dataOffset, value);
+
+    if (signed) {
+      this._dataView.setInt16(this._dataOffset, value);
+    } else {
+      this._dataView.setUint16(this._dataOffset, value);
+    }
     this._dataOffset += 2;
   }
 
-  private setUint32ToDataArea(value: number): void {
+  private setInt32ToDataArea(value: number, signed: boolean = false): void {
     if (this._dataOffset + 4 > this.size) {
       throw new RangeError("지정된 DataView의 크기를 초과했습니다.");
     }
-    this._dataView.setUint32(this._dataOffset, value);
+
+    if (signed) {
+      this._dataView.setInt32(this._dataOffset, value);
+    } else {
+      this._dataView.setUint32(this._dataOffset, value);
+    }
     this._dataOffset += 4;
   }
 
@@ -145,9 +119,9 @@ export class DataViewStub {
 
     const componentCount = value.length;
 
-    this.setUint16ToEntry(tag); // Tag
-    this.setUint16ToEntry(TagFormat.ASCIIString); // Format
-    this.setUint32ToEntry(componentCount); // Component's Count
+    this.setInt16ToEntry(tag); // Tag
+    this.setInt16ToEntry(TagFormat.ASCIIString); // Format
+    this.setInt32ToEntry(componentCount); // Component's Count
 
     const componentSize = COMPONENT_SIZE_BY_FORMAT[TagFormat.ASCIIString]; // 1 Byte
     const payloadSize = componentSize * componentCount;
@@ -161,11 +135,159 @@ export class DataViewStub {
     }
 
     if (payloadSize > 4) {
-      this.setUint32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
-      charCodes.forEach((code) => this.setUint8ToDataArea(code));
+      this.setInt32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
+      charCodes.forEach((code) => this.setInt8ToDataArea(code));
     } else {
-      charCodes.forEach((code) => this.setUint8ToEntry(code));
+      [...charCodes, ...Array(4 - componentCount).fill(0)].forEach((code) => this.setInt8ToEntry(code));
     }
+
+    this._currentEntryCount++;
+
+    return this;
+  }
+
+  appendSignedShortEntry(tag: number, values: number[]): this {
+    if (this._currentEntryCount >= this._maxEntryCount) {
+      throw new RangeError("지정된 엔트리 개수보다 더 많은 엔트리를 추가할 수 없습니다.");
+    }
+
+    const componentCount = values.length;
+
+    this.setInt16ToEntry(tag); // Tag
+    this.setInt16ToEntry(TagFormat.SignedShort); // Format
+    this.setInt32ToEntry(componentCount); // Component's Count
+
+    const componentSize = COMPONENT_SIZE_BY_FORMAT[TagFormat.SignedShort]; // 2 Bytes
+    const payloadSize = componentSize * componentCount;
+
+    if (payloadSize > 4) {
+      this.setInt32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
+      values.forEach((value) => this.setInt16ToDataArea(value, true));
+    } else {
+      [...values, ...Array(2 - componentCount).fill(0)].forEach((value) => this.setInt16ToEntry(value, true));
+    }
+
+    this._currentEntryCount++;
+
+    return this;
+  }
+
+  appendSignedLongEntry(tag: number, values: number[]): this {
+    if (this._currentEntryCount >= this._maxEntryCount) {
+      throw new RangeError("지정된 엔트리 개수보다 더 많은 엔트리를 추가할 수 없습니다.");
+    }
+
+    const componentCount = values.length;
+
+    this.setInt16ToEntry(tag); // Tag
+    this.setInt16ToEntry(TagFormat.SignedLong); // Format
+    this.setInt32ToEntry(componentCount); // Component's Count
+
+    const componentSize = COMPONENT_SIZE_BY_FORMAT[TagFormat.SignedLong]; // 4 Bytes
+    const payloadSize = componentSize * componentCount;
+
+    if (payloadSize > 4) {
+      this.setInt32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
+      values.forEach((value) => this.setInt32ToDataArea(value, true));
+    } else {
+      values.forEach((value) => this.setInt32ToEntry(value, true));
+    }
+
+    this._currentEntryCount++;
+
+    return this;
+  }
+
+  appendSignedRationalEntry(tag: number, values: [number, number][]): this {
+    if (this._currentEntryCount >= this._maxEntryCount) {
+      throw new RangeError("지정된 엔트리 개수보다 더 많은 엔트리를 추가할 수 없습니다.");
+    }
+
+    const componentCount = values.length;
+
+    this.setInt16ToEntry(tag); // Tag
+    this.setInt16ToEntry(TagFormat.SignedRational); // Format
+    this.setInt32ToEntry(componentCount); // Component's Count
+    this.setInt32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
+
+    values.forEach(([numerator, denominator]) => {
+      this.setInt32ToDataArea(numerator, true);
+      this.setInt32ToDataArea(denominator, true);
+    });
+
+    this._currentEntryCount++;
+
+    return this;
+  }
+
+  appendUnsignedShortEntry(tag: number, values: number[]): this {
+    if (this._currentEntryCount >= this._maxEntryCount) {
+      throw new RangeError("지정된 엔트리 개수보다 더 많은 엔트리를 추가할 수 없습니다.");
+    }
+
+    const componentCount = values.length;
+
+    this.setInt16ToEntry(tag); // Tag
+    this.setInt16ToEntry(TagFormat.UnsignedShort); // Format
+    this.setInt32ToEntry(componentCount); // Component's Count
+
+    const componentSize = COMPONENT_SIZE_BY_FORMAT[TagFormat.UnsignedShort]; // 2 Bytes
+    const payloadSize = componentSize * componentCount;
+
+    if (payloadSize > 4) {
+      this.setInt32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
+      values.forEach((value) => this.setInt16ToDataArea(value));
+    } else {
+      [...values, ...Array(2 - componentCount).fill(0)].forEach((value) => this.setInt16ToEntry(value));
+    }
+
+    this._currentEntryCount++;
+
+    return this;
+  }
+
+  appendUnsignedLongEntry(tag: number, values: number[]): this {
+    if (this._currentEntryCount >= this._maxEntryCount) {
+      throw new RangeError("지정된 엔트리 개수보다 더 많은 엔트리를 추가할 수 없습니다.");
+    }
+
+    const componentCount = values.length;
+
+    this.setInt16ToEntry(tag); // Tag
+    this.setInt16ToEntry(TagFormat.UnsignedLong); // Format
+    this.setInt32ToEntry(componentCount); // Component's Count
+
+    const componentSize = COMPONENT_SIZE_BY_FORMAT[TagFormat.UnsignedLong]; // 4 Bytes
+    const payloadSize = componentSize * componentCount;
+
+    if (payloadSize > 4) {
+      this.setInt32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
+      values.forEach((value) => this.setInt32ToDataArea(value));
+    } else {
+      values.forEach((value) => this.setInt32ToEntry(value));
+    }
+
+    this._currentEntryCount++;
+
+    return this;
+  }
+
+  appendUnsignedRationalEntry(tag: number, values: [number, number][]): this {
+    if (this._currentEntryCount >= this._maxEntryCount) {
+      throw new RangeError("지정된 엔트리 개수보다 더 많은 엔트리를 추가할 수 없습니다.");
+    }
+
+    const componentCount = values.length;
+
+    this.setInt16ToEntry(tag); // Tag
+    this.setInt16ToEntry(TagFormat.UnsignedRational); // Format
+    this.setInt32ToEntry(componentCount); // Component's Count
+    this.setInt32ToEntry(this._dataOffset - this.BYTE_ALIGN_OFFSET); // NOTE: 데이터 오프셋은 원본 값을 저장한다.
+
+    values.forEach(([numerator, denominator]) => {
+      this.setInt32ToDataArea(numerator);
+      this.setInt32ToDataArea(denominator);
+    });
 
     this._currentEntryCount++;
 
--- src/models/ExifData/ImageFileDirectory/specs/ImageFileDirectory.model.spec.ts
+++ src/models/ExifData/ImageFileDirectory/specs/ImageFileDirectory.model.spec.ts
@@ -2,16 +2,108 @@ import { ImageFileDirectory } from "../ImageFileDirectory.model";
 import { DataViewStub } from "./DataViewStub";
 
 describe("IFD 클래스의 동작을 테스트한다.", () => {
-  it("문자열 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
-    const dataViewStub = new DataViewStub(2, 50);
+  it("문자열 타입의 데이터를 가진 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
+    const dataViewStub = new DataViewStub(3, 100);
 
     dataViewStub
       .appendStringEntry(0x010f, "Apple\0") // Make
+      .appendStringEntry(0x0131, "iOS") // Software
       .appendStringEntry(0x0110, "iPhone 13 Pro"); // Model
 
     const imageFileDirectory = new ImageFileDirectory(dataViewStub.dataView, 0, false);
 
     expect(imageFileDirectory.entries[0].payload).toBe("Apple");
-    expect(imageFileDirectory.entries[1].payload).toBe("iPhone 13 Pro");
+    expect(imageFileDirectory.entries[1].payload).toBe("iOS");
+    expect(imageFileDirectory.entries[2].payload).toBe("iPhone 13 Pro");
+  });
+
+  it("Signed Short 타입의 데이터를 가진 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
+    const dataViewStub = new DataViewStub(3, 100);
+
+    dataViewStub
+      .appendSignedShortEntry(0x0001, [10, 11, 12, 13, 14, 15])
+      .appendSignedShortEntry(0x0002, [-10])
+      .appendSignedShortEntry(0x0002, [-100, -110, -120, -130]);
+
+    const imageFileDirectory = new ImageFileDirectory(dataViewStub.dataView, 0, false);
+
+    expect(imageFileDirectory.entries[0].payload).toEqual([10, 11, 12, 13, 14, 15]);
+    expect(imageFileDirectory.entries[1].payload).toBe(-10);
+    expect(imageFileDirectory.entries[2].payload).toEqual([-100, -110, -120, -130]);
+  });
+
+  it("Signed Long 타입의 데이터를 가진 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
+    const dataViewStub = new DataViewStub(3, 100);
+
+    dataViewStub
+      .appendSignedLongEntry(0x0001, [286331153, 286331151, 276331158, 246331153, 286331159])
+      .appendSignedLongEntry(0x0002, [-321])
+      .appendSignedLongEntry(0x0002, [-100000, -110000, -120000, -130000]);
+
+    const imageFileDirectory = new ImageFileDirectory(dataViewStub.dataView, 0, false);
+
+    expect(imageFileDirectory.entries[0].payload).toEqual([286331153, 286331151, 276331158, 246331153, 286331159]);
+    expect(imageFileDirectory.entries[1].payload).toBe(-321);
+    expect(imageFileDirectory.entries[2].payload).toEqual([-100000, -110000, -120000, -130000]);
+  });
+
+  it("Signed Rational 타입의 데이터를 가진 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
+    const dataViewStub = new DataViewStub(3, 100);
+
+    dataViewStub
+      .appendSignedRationalEntry(0x0001, [[-299, 1000]])
+      .appendSignedRationalEntry(0x0002, [[9212, 1000]])
+      .appendSignedRationalEntry(0x0002, [[-982, 1000]]);
+
+    const imageFileDirectory = new ImageFileDirectory(dataViewStub.dataView, 0, false);
+
+    expect(imageFileDirectory.entries[0].payload).toBe(-0.299);
+    expect(imageFileDirectory.entries[1].payload).toBe(9.212);
+    expect(imageFileDirectory.entries[2].payload).toBe(-0.982);
+  });
+
+  it("Unsigned Short 타입의 데이터를 가진 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
+    const dataViewStub = new DataViewStub(3, 100);
+
+    dataViewStub
+      .appendUnsignedShortEntry(0x0001, [1000, 1001, 1002, 1003, 1004])
+      .appendUnsignedShortEntry(0x0002, [2000])
+      .appendUnsignedShortEntry(0x0002, [3000, 3001, 3002, 3003]);
+
+    const imageFileDirectory = new ImageFileDirectory(dataViewStub.dataView, 0, false);
+
+    expect(imageFileDirectory.entries[0].payload).toEqual([1000, 1001, 1002, 1003, 1004]);
+    expect(imageFileDirectory.entries[1].payload).toBe(2000);
+    expect(imageFileDirectory.entries[2].payload).toEqual([3000, 3001, 3002, 3003]);
+  });
+
+  it("Unsigned Long 타입의 데이터를 가진 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
+    const dataViewStub = new DataViewStub(3, 100);
+
+    dataViewStub
+      .appendUnsignedLongEntry(0x0001, [286331153, 286331151, 276331158, 246331153, 286331159])
+      .appendUnsignedLongEntry(0x0002, [1000])
+      .appendUnsignedLongEntry(0x0002, [100000, 110000, 120000, 130000]);
+
+    const imageFileDirectory = new ImageFileDirectory(dataViewStub.dataView, 0, false);
+
+    expect(imageFileDirectory.entries[0].payload).toEqual([286331153, 286331151, 276331158, 246331153, 286331159]);
+    expect(imageFileDirectory.entries[1].payload).toBe(1000);
+    expect(imageFileDirectory.entries[2].payload).toEqual([100000, 110000, 120000, 130000]);
+  });
+
+  it("Unsigned Rational 타입의 데이터를 가진 IFD Entry로 구성된 Image File Directory의 데이터를 읽는다.", () => {
+    const dataViewStub = new DataViewStub(3, 100);
+
+    dataViewStub
+      .appendUnsignedRationalEntry(0x0001, [[9128, 100]])
+      .appendUnsignedRationalEntry(0x0002, [[91, 10000]])
+      .appendUnsignedRationalEntry(0x0002, [[55850, 10]]);
+
+    const imageFileDirectory = new ImageFileDirectory(dataViewStub.dataView, 0, false);
+
+    expect(imageFileDirectory.entries[0].payload).toBe(91.28);
+    expect(imageFileDirectory.entries[1].payload).toBe(0.0091);
+    expect(imageFileDirectory.entries[2].payload).toBe(5585);
   });
 });
